31b10e2c9ea5e26ce4649315e09ad8c782409171
jianblog <jimberg@126.com>
2019-11-28 17:18:34 +0800

    单链表: 删除倒数第n节点

--- linkList/main.py
+++ linkList/main.py
@@ -3,6 +3,7 @@
 import node
 
 def newLink(s):
+    # 使用字符串初始化一个链表
     thisLink = node.LinkNode()
     for i in str(s):
         thisLink.addNode(node.Node(i))
@@ -11,20 +12,20 @@ def newLink(s):
 def basicOperation():
     thisLink = newLink("world")
     # 追加节点
-    # thisLink.addNode(node.Node("A"))
-    # print(thisLink)
+    thisLink.addNode(node.Node("A"))
+    print(thisLink)
 
     # 头部插入节点
-    # thisLink.insertNode("w", node.Node("1"))
-    # print(thisLink)
+    thisLink.insertNode("w", node.Node("1"))
+    print(thisLink)
 
     # 任意位插入
-    # thisLink.insertNode("r", node.Node("2"))
-    # print(thisLink)
+    thisLink.insertNode("r", node.Node("2"))
+    print(thisLink)
 
     # 删除节点
-    # thisLink.deleteNode("w")
-    # print(thisLink)
+    thisLink.deleteNode("w")
+    print(thisLink)
     return thisLink
 
 
@@ -91,8 +92,10 @@ def mergeLink(la, lb):
     if not ptb:
         return la
 
+    # 定位最终单链表的起始, 路径移动过程
     pNode = None
     p_tail  = None
+
     # 定位起始
     if pta.value < ptb.value:
         pNode = la
@@ -104,6 +107,7 @@ def mergeLink(la, lb):
         ptb = ptb.next
 
     while True:
+        # 循环终止条件
         if not pta:
             p_tail.next = ptb
             break
@@ -121,6 +125,32 @@ def mergeLink(la, lb):
             ptb = ptb.next
     return pNode
 
+def deleteLastN(nl, n):
+    # 删除倒数第n个节点
+    pta = nl.head
+    ptb = nl.head
+
+    # 让一个指针先前进n步
+    dis = 0
+    for i in range(n):
+        if pta and pta.next:
+            pta = pta.next
+            dis += 1
+    if dis < n - 1:
+        return nl
+    elif dis == n - 1:
+        nl.head = ptb.next
+    else:
+        while pta.next:
+            pta = pta.next
+            ptb = ptb.next
+        if ptb.next.next:
+            ptb.next = ptb.next.next
+        else:
+            ptb.next = None
+    return nl
+
+
 
 if __name__ == "__main__":
     link =  basicOperation()
@@ -134,8 +164,15 @@ if __name__ == "__main__":
 
 
     # 链表合并
+    print("链表合并")
     la = newLink("ehr")
     lb = newLink("acnq")
 
     c = mergeLink(la, lb)
     print(c)
+    print("\n")
+
+    n = 5
+    print("删除倒数第%d个" % n)
+    d = newLink("world")
+    print(deleteLastN(d, n))
